3418b58e5f5ed29080d1457c1b98ca38b48b183d
Cristiano Estauber <cristianoestauber@gmail.com>
2022-10-19 20:51:33 -0300

    Primeira tela criada

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+
+import javax.swing.JFrame;
+
+public class Program extends JFrame {
+
+    public static void main(String[] args) {
+
+        JFrame janela = new JFrame();
+
+        janela.setBounds(150, 35, 250, 350);
+        janela.setVisible(true);
+
+    }
+}
