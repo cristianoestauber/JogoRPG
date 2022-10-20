83ba65fee923f7fe5cf28a4776722bc66d60bf24
Cristiano Estauber <cristianoestauber@gmail.com>
2022-10-20 15:05:37 -0300

    Criando Status de personagem

--- src/telas/TelaPrincipal.java
+++ src/telas/TelaPrincipal.java
@@ -1,6 +1,7 @@
 package telas;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TelaPrincipal extends JFrame {
 
@@ -10,6 +11,33 @@ public class TelaPrincipal extends JFrame {
         JPanel painel = new JPanel();
         this.add(painel);
         painel.setLayout(null);
+
+        JPanel personagem = new JPanel();
+        painel.add(personagem);
+        personagem.setLayout(null);
+        personagem.setBackground(Color.LIGHT_GRAY);
+        personagem.setBounds(8, 10, 200, 50);
+
+        JLabel status = new JLabel("STATUS");
+        personagem.add(status);
+        status.setBackground(Color.WHITE);
+        status.setBounds(75, 1, 50, 14);
+
+        JLabel vida = new JLabel("VIDA: ");
+        personagem.add(vida);
+        vida.setBounds(5, 15, 50, 18);
+
+        JLabel mana = new JLabel("MANA: ");
+        personagem.add(mana);
+        mana.setBounds(5, 30, 50, 18);
+
+        JLabel xp = new JLabel("EXP: ");
+        personagem.add(xp);
+        xp.setBounds(115, 15, 50, 18);
+
+        JLabel level = new JLabel("LEVEL: ");
+        personagem.add(level);
+        level.setBounds(115, 30, 50, 18);
 
         JButton botaoAtacar = new JButton("Atacar");
         painel.add(botaoAtacar);
