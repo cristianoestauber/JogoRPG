9a2b1459d76930f19a86f84e2988c53075d7145f
Cristiano Estauber <cristianoestauber@gmail.com>
2022-10-19 21:01:00 -0300

    Criando Botoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Program extends JFrame {
 
@@ -9,6 +9,19 @@ public class Program extends JFrame {
 
         JFrame janela = new JFrame();
 
+        JPanel painel = new JPanel();
+        janela.add(painel);
+
+        JButton botao = new JButton("Button 1");
+        painel.add(botao);
+        JButton botao2 = new JButton("Button 2");
+        painel.add(botao2);
+        JButton botao3 = new JButton("Button 3");
+        painel.add(botao3);
+        JButton botao4 = new JButton("Button 4");
+        painel.add(botao4);
+
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setBounds(150, 35, 250, 350);
         janela.setVisible(true);
 
